287990d0ac53b420097c56c5ca469522b93f563f
oleg <olegvanich@gmail.com>
2018-11-03 16:03:04 +0200

    initial commit

---
+++ src/main/java/oleg/podolian/demo/service/ParsingService.java
@@ -0,0 +1,8 @@
+package oleg.podolian.demo.service;
+
+import java.io.File;
+
+public interface ParsingService {
+
+    boolean parse(File file);
+}
---
+++ src/main/java/oleg/podolian/demo/service/impl/ParsingServiceImpl.java
@@ -0,0 +1,12 @@
+package oleg.podolian.demo.service.impl;
+
+import oleg.podolian.demo.service.ParsingService;
+
+import java.io.File;
+
+public class ParsingServiceImpl implements ParsingService {
+    @Override
+    public boolean parse(File file) {
+        return false;
+    }
+}
