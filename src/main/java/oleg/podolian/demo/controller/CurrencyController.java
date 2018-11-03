5da71ca3737e2c7b71822691be1235efd1025f6b
oleg <olegvanich@gmail.com>
2018-11-03 16:08:58 +0200

    initial commit 2

--- src/main/java/oleg/podolian/demo/controller/CurrencyController.java
+++ src/main/java/oleg/podolian/demo/controller/CurrencyController.java
@@ -1,25 +1,33 @@
 package oleg.podolian.demo.controller;
 
+import oleg.podolian.demo.model.Currency;
 import oleg.podolian.demo.service.CurrencyService;
+import oleg.podolian.demo.service.ParsingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/aggregator")
 public class CurrencyController {
 
     private final CurrencyService currencyService;
+    private final ParsingService parsingService;
 
     @Autowired
-    public CurrencyController(CurrencyService currencyService) {
+    public CurrencyController(CurrencyService currencyService,
+                              ParsingService parsingService) {
         this.currencyService = currencyService;
+        this.parsingService = parsingService;
     }
 
     @GetMapping("/hello/{name}")
@@ -35,8 +43,20 @@ public class CurrencyController {
             System.out.println("Not found");
 //        return ResponseEntity.ok();
         } else {
-            System.out.println(file.getOriginalFilename());
+//            boolean result = parsingService.parse(file)
+            if (true) {
+//                return; ok
+            } else {
+//                bad file
+            }
         }
 //        return ResponseEntity.ok();
+    }
+
+    @GetMapping("/currencies/{bank}")
+    @ResponseBody
+    public List<Currency> currencies(@PathVariable String bank) {
+        return Collections.emptyList();
+//        return currencyService.fi
     }
 }
--- src/main/java/oleg/podolian/demo/service/impl/ParsingServiceImpl.java
+++ src/main/java/oleg/podolian/demo/service/impl/ParsingServiceImpl.java
@@ -1,12 +1,15 @@
 package oleg.podolian.demo.service.impl;
 
 import oleg.podolian.demo.service.ParsingService;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 
+@Service
 public class ParsingServiceImpl implements ParsingService {
     @Override
     public boolean parse(File file) {
+
         return false;
     }
 }
